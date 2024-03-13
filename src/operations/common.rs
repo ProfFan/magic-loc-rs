026868e111d0c846fa62e17ebd1a68997b7f62df
Fan Jiang <i@fanjiang.me>
2024-03-12 20:32:57 -0400

    Add fp_index check

--- src/operations/common.rs
+++ src/operations/common.rs
@@ -56,6 +56,31 @@ where
 
         let (msg_length, rx_time) = result.unwrap();
 
+        // Check IP_TOAST to ensure the ToA status is good
+        // TODO: Move this out of the listen_for_packet function
+        let ip_toast = rxing.ll().ip_ts().read().unwrap().ip_toast();
+        if ip_toast != 0 {
+            defmt::error!("IP_TOAST: {}", ip_toast);
+
+            rxing.force_idle().unwrap();
+            dw3000 = rxing.finish_receiving().unwrap();
+            return (dw3000, Err(()));
+        }
+
+        // Check FP_INDEX to ensure the frame is valid
+        let fp_index = rxing.ll().ip_diag_8().read().unwrap().ip_fp();
+
+        // fp_index is a 16-bit fixed point number, 10.6 format
+        // The integer part is the first 10 bits, and the fractional part is the last 6 bits
+        let fp_index_int = fp_index >> 6;
+        if fp_index_int < 730 && fp_index_int > 0 {
+            defmt::warn!("FP_INDEX DETECTED AT: {}", fp_index >> 6);
+
+            rxing.force_idle().unwrap();
+            dw3000 = rxing.finish_receiving().unwrap();
+            return (dw3000, Err(()));
+        }
+
         callback(&buf[..msg_length], rx_time);
     } else {
         defmt::debug!("Timeout waiting for packet!");
